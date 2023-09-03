9fe220b97d674a43cda4ee69f1a6170986dcbed0
Junghyeon <tlslrs1994@gmail.com>
2023-09-03 16:52:07 +0900

    chore : namespace 설정
    - TestMonsterController.cs에 namespace 설정

--- Heroes_vs_Hordes/Assets/Prototype/Scripts/TestMonsterController.cs
+++ Heroes_vs_Hordes/Assets/Prototype/Scripts/TestMonsterController.cs
@@ -1,32 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TestMonsterController : MonoBehaviour
+namespace ProtoType
 {
-    [SerializeField] Transform _target;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-    private Rigidbody2D _rigid;
-
-    private const float REVERSE_ANGLE = -1f;
-    private const float CHECK_DIRECTION = 0f;
-
-    private void Awake()
+    public class TestMonsterController : MonoBehaviour
     {
-        _rigid = GetComponent<Rigidbody2D>();
-    }
+        [SerializeField] Transform _target;
 
-    private void FixedUpdate()
-    {
-        var enemyToHeroVec = _target.position - transform.position;
-        var lookAngle = Vector2.Angle(Vector2.up, new Vector2(enemyToHeroVec.x, enemyToHeroVec.y).normalized);
-        if (_IsLocatedTargetRightSide(enemyToHeroVec.x))
-            lookAngle *= REVERSE_ANGLE;
-        _rigid.rotation = lookAngle;
-    }
+        private Rigidbody2D _rigid;
 
-    private bool _IsLocatedTargetRightSide(float value)
-    {
-        return value >= CHECK_DIRECTION;
+        private const float REVERSE_ANGLE = -1f;
+        private const float CHECK_DIRECTION = 0f;
+
+        private void Awake()
+        {
+            _rigid = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            var enemyToHeroVec = _target.position - transform.position;
+            var lookAngle = Vector2.Angle(Vector2.up, new Vector2(enemyToHeroVec.x, enemyToHeroVec.y).normalized);
+            if (_IsLocatedTargetRightSide(enemyToHeroVec.x))
+                lookAngle *= REVERSE_ANGLE;
+            _rigid.rotation = lookAngle;
+        }
+
+        private bool _IsLocatedTargetRightSide(float value)
+        {
+            return value >= CHECK_DIRECTION;
+        }
     }
 }
