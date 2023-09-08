c549366b67ffbe55efb9afb3b0090ff44c05c4af
Junghyeon <tlslrs1994@gmail.com>
2023-09-08 10:54:44 +0900

    feat : UI_MainScene 추가 구현
    - 플레이 버튼 클릭시 UI_IngameScene 을 보여주며 인게임 시작

--- Heroes_vs_Hordes/Assets/Scripts/UIs/UI_Scene/UI_MainScene.cs
+++ Heroes_vs_Hordes/Assets/Scripts/UIs/UI_Scene/UI_MainScene.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UI_MainScene : UI_Scene
@@ -11,18 +12,38 @@ public class UI_MainScene : UI_Scene
         PrevButton
     }
 
+    private enum ETexts
+    {
+        ChapterText
+    }
+
+    private GameObject _nextButton;
+    private GameObject _prevButton;
+    private TextMeshProUGUI _chapterText;
+
+    private const int FIRST_WAVE_INDEX = 0;
+
     protected override void _Init()
     {
         _BindButton(typeof(EButtons));
+        _BindText(typeof(ETexts));
+
+        _nextButton = _GetButton((int)EButtons.NextButton).gameObject;
+        _prevButton = _GetButton((int)EButtons.PrevButton).gameObject;
 
         _BindEvent(_GetButton((int)EButtons.PlayButton).gameObject, _PlayChapter);
-        _BindEvent(_GetButton((int)EButtons.NextButton).gameObject, _ShowNextChapter);
-        _BindEvent(_GetButton((int)EButtons.PrevButton).gameObject, _ShowPrevChapter);
+        _BindEvent(_nextButton, _ShowNextChapter);
+        _BindEvent(_prevButton, _ShowPrevChapter);
+
+        _chapterText = _GetText((int)ETexts.ChapterText);
     }
 
     private void _PlayChapter()
     {
-
+        Manager.Instance.UI.ShowSceneUI<UI_IngameScene>(Define.RESOURCE_UI_INGAME_SCENE, (ingameSceneUI) =>
+        {
+            Manager.Instance.Ingame.StartIngame(FIRST_WAVE_INDEX);
+        });
     }
 
     private void _ShowNextChapter()
